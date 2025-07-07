ea6273ef13c2ed9a21a3e74f892b76b450cd881d
Marvin <sonmarvint2001@gmail.com>
2025-07-06 22:19:03 -0600

    Clases terminadas

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+ClientesList=[]
+CitasList=[]
 class Mascota:
     def __init__(self,nombre,edad,peso,especie,dueño):
         self.nombre = nombre
@@ -52,4 +54,41 @@ class CitaMedica:
     def Mostrar(self):
         print(f"Descripcion: {self.descripcion}")
         self.mascota.Mostrar()
+def IngresoCliente():
+    while True:
+        try:
+            existe=False
+            nombreAux=input("Ingrese nombre: ")
+            cuiAux=int(input("Ingrese cui: "))
+            for cl in ClientesList:
+                if cl.cui == cuiAux:
+                    existe=True
+            telefonoAux=int(input("Ingrese telefono: "))
+            ClienteAux=Cliente(nombreAux,cuiAux,telefonoAux)
+            if not existe:
+                ClientesList.append(ClienteAux)
+                print("Cliente creado con exito")
+                break
+            else:
+                print("Cliente ya existe")
+                opcionAux = input("Regresar al menu principal? s/n: ").lower()
+                if opcionAux == "s":
+                    break
+        except ValueError:
+            print("ERROR: CUI Y TElEFONO deben ser numericos")
+            opcionAux= input("Regresar al menu principal? s/n: ").lower()
+            if opcionAux == "s":
+                break
 
+
+while True:
+    print("==MENU==")
+    print("1. Ingresar Cliente")
+    print("2. Ingresar Mascota")
+    print("3. Ingresar Cita Medica")
+    print("4. Ver clientes")
+    print("5. Ver Citas programadas")
+    opcion=input("Ingrese una opcion: ")
+    match opcion:
+        case "1":
+            IngresoCliente()
